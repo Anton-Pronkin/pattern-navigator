35d5961df3d6a944c263f6916efd5a752d7db56d
Anton Pronkin <Anton_Pronkin@epam.com>
2021-04-09 23:20:42 +0300

    Introduce vue

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,30 @@
 const path = require('path');
+const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
     entry: {
         content: './src/scripts/content.js',
         background: './src/scripts/background.js'
+        options: "./src/scripts/options.js"
     },
     mode: "development",
     devtool: "source-map",
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            exclude: /node_modules/,
+            loader: 'vue-loader'
+        }, {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: ["vue-style-loader", "css-loader"]
+        }],
+    },
     output: {
         filename: '[name].min.js',
         path: path.resolve(__dirname, 'app/scripts'),
-    }
+    },
+    plugins: [
+      new VueLoaderPlugin()
+    ]
 };
