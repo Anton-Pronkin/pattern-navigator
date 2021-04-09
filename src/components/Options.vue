98d4a1416d50115eb21012bd8ca324fe4a520a75
Anton Pronkin <Anton_Pronkin@epam.com>
2021-04-09 23:39:55 +0300

    Add App component

---
+++ src/components/Options.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="options">Options page</div>
+</template>
+
+<script>
+export default {
+  name: "Options",
+};
+</script>
+
+<style>
+.options {
+  text-align: center;
+}
+</style>
